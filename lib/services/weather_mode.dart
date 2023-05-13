5ae24b1563ec45f3332da38180c7575c9eec5492
Yousif Omar <yousifomar110@gmail.com>
2023-05-13 15:00:03 +0200

    weather_mode_1

---
+++ lib/services/weather_mode.dart
@@ -0,0 +1,11 @@
+Class WeatherModel {
+  String date;
+  double temp;
+  double maxTemp;
+  double minTemp;
+  String weatherState;
+  String weatherStateIcon;
+WeatherModel({
+  required this.
+})
+}
